04cb679747ca824f2878e0e8a3434442342488a4
BrettBlox <Diamond5.12>
2019-04-16 20:27:35 -0600

    added apollo packages

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,9 +1,16 @@
-import React from "react";
-import { Meteor } from "meteor/meteor";
-import { render } from "react-dom";
+import React from 'react'
+import { Meteor } from 'meteor/meteor'
+import { render } from 'react-dom'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-import App from "../../ui/App";
+import App from '../../ui/App'
+
+const httpLink = new HttpLink({
+  uri: Meteor.absoluteUrl('graphql')
+})
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById("app"));
-});
+  render(<App />, document.getElementById('app'))
+})
