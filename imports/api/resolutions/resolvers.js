0b43caacccd8c72259b3d17bd1ce5960330158d9
BrettBlox <Diamond5.12>
2019-05-02 18:40:48 -0600

    made apollo aware of login token

--- imports/api/resolutions/resolvers.js
+++ imports/api/resolutions/resolvers.js
@@ -2,13 +2,13 @@ import Resolutions from './resolutions'
 
 export default {
   Query: {
-    resolutions() {
+    resolutions(obj, args, context) {
+      console.log(context)
       return Resolutions.find({}).fetch()
     }
   },
   Mutation: {
     createResolution(obj, { name }, context) {
-      console.log(name)
       const resolutionId = Resolutions.insert({
         name
       })
